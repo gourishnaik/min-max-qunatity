0dfc28855724a5743c1fbecea35a425199e83642
gourish naik <gourish@campionsoftware.com>
2022-09-02 14:31:45 +0530

    commited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'minmax';
+ count:number = 0;
+ maximum= false;
+ minimum = false;
+  constructor() { }
+
+  
+  ngOnInit(): void {
+  }
+
+increase(){
+  this.minimum = false;
+  if(this.count<3){
+    this.count = this.count +3;
+  }
+  else{
+    this.count = this.count +1;
+  }
+
+
+  if(this.count>10){
+    this.maximum =true;
+    this.count = this.count =10;
+  }
+
+}
+
+decrease(){
+this.count = this.count -1;
+this.maximum =false;
+
+if(this.count<3){
+
+  this.minimum = true;
+  this.count = this.count =0;
+}
+
+}
+
+
 }
